3b1d3ef9d02eff822fd5df0db731e979c0650152
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2023-01-01 22:12:02 -0600

    Initial commit

---
+++ scripts/generate-markdown.py
@@ -0,0 +1,67 @@
+def main():
+    file_out = open('../docs/README.md', 'w', encoding='utf-8')
+
+    file_template = open('../sources/template.md', 'r', encoding='utf-8')
+
+    line = file_template.readline()
+    while line:
+        if line.startswith('{{main_content}}'):
+            generateMainContent(file_out)
+        else:
+            file_out.write(line)
+        line = file_template.readline()
+
+    file_out.close()
+    file_template.close()
+
+
+def generateMainContent(file_out):
+    file_in = open('../sources/theotokarion.txt', 'r', encoding='utf-8')
+
+    line = file_in.readline()
+
+    ode = False
+    evening = False
+
+    while line:
+        line = line.rstrip()
+        if line.startswith('Ode'):
+            line = '#### ' + line
+            ode = True
+            evening = False
+        elif line.endswith('Prosomia'):
+            line = '#### ' + line
+            ode = True
+            evening = False
+        elif line.endswith('Kathisma'):
+            line = '#### ' + line
+            ode = True
+            evening = False
+        elif line.endswith('Evening'):
+            line = '### ' + line
+            ode = False
+            evening = True
+        elif line.endswith('Tone'):
+            line = '## ' + line
+            ode = False
+            evening = False
+        elif ode and len(line) > 0:
+            line = f'<p class="ode">{line}</p>'
+        elif evening and len(line) > 0:
+            line = f'<p class="evening-subtitle">{line}</p>'
+
+        line += '\n'
+
+        file_out.write(line)
+        line = file_in.readline()
+
+    file_in.close()
+
+
+if __name__ == "__main__":
+    main()
+
+# Title
+# Tone
+# Day
+# ode
---
+++ sources/template.md
@@ -0,0 +1,27 @@
+# Theotokarion
+
+<p class="subtitle">Canons for the Mother of God</p>
+
+<p class="subtitle">Compiled by St. Nicodemus the Hagiorite</p>
+
+{{main_content}}
+
+<style>
+p.ode:first-letter {
+    color: #880000;
+    font-size: 180%;
+    font-weight: normal;
+}
+
+h1, h2, h3, h4, h5, h6 {
+    text-align: center;
+}
+
+p.subtitle {
+    text-align: center;
+}
+
+p.evening-subtitle {
+    text-align: center;
+}
+</style>
